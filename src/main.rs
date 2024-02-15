a1c1b04d0bd442a98a72ae52bb9c155039c8b44d
harshraj2717 <harshraj2717@gmail.com>
2024-02-16 03:34:02 +0530

    Implement REPL
    read-execute-print loop (the first thing SQLite runs whwn running it in
    terminal)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,99 @@
+struct InputBuffer {
+    buffer: String,
+}
+
+impl InputBuffer {
+    fn new() -> Self {
+        Self {
+            buffer: String::new(),
+        }
+    }
+
+    fn clear(&mut self) {
+        self.buffer = String::new();
+    }
+
+    fn print_prompt(&self, multi_line: bool) {
+        if multi_line {
+            print!("       > ");
+        } else {
+            print!("rsqlite> ");
+        }
+        match std::io::Write::flush(&mut std::io::stdout()) {
+            Ok(_) => {}
+            Err(error) => {
+                println!("\nFailed to flush output : {error}");
+                std::process::exit(1);
+            }
+        }
+    }
+
+    fn read_multiline(&self, mut multi_line_input: String) -> String {
+        loop {
+            if let Some(last_char) = multi_line_input.chars().last() {
+                if last_char == ';' {
+                    break;
+                }
+            }
+            self.print_prompt(true);
+            let mut cur_input = String::new();
+            match std::io::stdin().read_line(&mut cur_input) {
+                Ok(_) => {
+                    cur_input = "\n".to_string() + cur_input.trim();
+                    multi_line_input.push_str(&cur_input);
+                }
+                Err(error) => {
+                    println!("\nFailed to read input : {error}");
+                    std::process::exit(1);
+                }
+            }
+        }
+        multi_line_input
+    }
+
+    fn read(&mut self) {
+        let mut input = String::new();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                input = input.trim().to_string();
+                if let Some(first_char) = input.chars().next() {
+                    if first_char != '.' {
+                        // SQL queries end with a ';' and can be multi-line
+                        // while SQLite commands start with a '.' and can not be multi-line
+                        // here I just get the multi-line input from user for queries
+                        input = self.read_multiline(input);
+                    }
+                    self.buffer = input;
+                }
+            }
+            Err(error) => {
+                println!("\nFailed to read input : {error}");
+                std::process::exit(1);
+            }
+        }
+    }
+
+    fn get(&self) -> String {
+        self.buffer.clone()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_buffer = InputBuffer::new();
+
+    loop {
+        input_buffer.print_prompt(false);
+        input_buffer.read();
+
+        let cur_buffer = input_buffer.get();
+        if cur_buffer == "" {
+            continue;
+        }
+        if cur_buffer == ".exit" {
+            std::process::exit(0);
+        }
+
+        println!("\n~~~\nUnrecognized command `{cur_buffer}` .\n~~~\n");
+        input_buffer.clear();
+    }
 }
